961b63ca56495191ab162c18c5f5880046f432c6
RIO-T-PRO <malibaalex42@gmail.com>
2025-12-07 23:21:48 +0300

    Home page Design

---
+++ src/app/components/layout/footer.tsx
@@ -0,0 +1,125 @@
+const Footer = () => {
+  return (
+    <footer className="bg-background-dark text-text-light px-6 lg:px-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div>
+          <div className="flex items-center gap-4 mb-3">
+            <div className="size-6 text-text-light">
+              <svg
+                fill="currentColor"
+                viewBox="0 0 48 48"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M42.4379 44C42.4379 44 36.0744 33.9038 41.1692 24C46.8624 12.9336 42.2078 4 42.2078 4L7.01134 4C7.01134 4 11.6577 12.932 5.96912 23.9969C0.876273 33.9029 7.27094 44 7.27094 44L42.4379 44Z"></path>
+              </svg>
+            </div>
+            <h2 className="text-text-light text-xl font-bold">ShopRiot</h2>
+          </div>
+          <p className="text-sm text-text-light">
+            Elevate Your Wardrobe with Irresistible Fashion Finds!
+          </p>
+        </div>
+
+        <div>
+          <h3 className="text-text-light font-semibold mb-3">Quick Links</h3>
+          <ul className="space-y-2 text-sm text-primary">
+            <li>
+              <a
+                className="hover:text-secondary-blue transition-colors"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-secondary-blue transition-colors"
+                href="#"
+              >
+                Catalog
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-secondary-blue transition-colors"
+                href="#"
+              >
+                Our Store
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-secondary-blue transition-colors"
+                href="#"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="text-text-light font-semibold mb-4">
+            Customer Service
+          </h3>
+          <ul className="space-y-2 text-sm">
+            <li>
+              <a
+                className="hover:text-secondary-blue transition-colors"
+                href="#"
+              >
+                FAQ
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-secondary-blue transition-colors"
+                href="#"
+              >
+                Shipping & Returns
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-secondary-blue transition-colors"
+                href="#"
+              >
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-secondary-blue transition-colors"
+                href="#"
+              >
+                Terms of Service
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="text-text-light font-semibold mb-4">Newsletter</h3>
+          <p className="text-sm mb-4 text-text-light">
+            Subscribe to our newsletter to get the latest updates.
+          </p>
+          <form className="flex">
+            <input
+              className="w-full rounded-l-lg outline-none border border-gray-600 bg-background-dark text-text-light pl-4 py-2"
+              placeholder="Your email"
+              type="email"
+            />
+            <button
+              className="bg-secondary-blue text-text-light px-4 rounded-r-lg transition-colors"
+              type="submit"
+            >
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/layout/header.tsx
@@ -0,0 +1,68 @@
+import { Heart, Home, ShoppingCart, User } from "lucide-react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between whitespace-nowrap px-6 lg:px-10 py-3 sticky top-0 z-10 bg-background-dark backdrop:blur-sm">
+      <div className="flex items-center gap-8">
+        <div className="flex items-center gap-2">
+          <div className="size-6 text-text-light">
+            <svg
+              fill="currentColor"
+              viewBox="0 0 48 48"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M42.4379 44C42.4379 44 36.0744 33.9038 41.1692 24C46.8624 12.9336 42.2078 4 42.2078 4L7.01134 4C7.01134 4 11.6577 12.932 5.96912 23.9969C0.876273 33.9029 7.27094 44 7.27094 44L42.4379 44Z"></path>
+            </svg>
+          </div>
+
+          <h2 className="text-text-light text-xl font-bold tracking-[-0.015]">
+            ShopRiot
+          </h2>
+        </div>
+
+        <nav className="flex items-center gap-6 text-primary font-medium">
+          <Link href="/" className="hover:text-secondary-blue">
+            Home
+          </Link>
+
+          <Link href="#" className="hover:text-secondary-blue">
+            Products
+          </Link>
+          <Link href="#" className="hover:text-secondary-blue">
+            About
+          </Link>
+          <Link href="#" className="hover:text-secondary-blue">
+            Contact
+          </Link>
+        </nav>
+      </div>
+
+      <div className="flex items-center gap-4.5">
+        <button className="flex items-center justify-center rounded-lg px-4 h-10 bg-secondary-blue whitespace-nowrap text-text-light cursor-pointer">
+          Sign up
+        </button>
+        <button className="flex items-center justify-center rounded-lg px-3 h-10 bg-transparent text-gray-600 dark:text-gray-300 hover:bg-gray-500/10 border border-gray-500 cursor-pointer">
+          <Heart size={18} />
+        </button>
+        <button className="relative flex items-center justify-center rounded-lg h-10 bg-transparent text-gray-600 dark:text-gray-300 hover:bg-gray-500/10 px-3 border border-gray-500 cursor-pointer">
+          <ShoppingCart size={18} />
+          <span className="absolute -top-1 -right-1 flex h-4 w-4 items-center justify-center rounded-full bg-secondary-blue text-[10px] font-bold text-text-light">
+            3
+          </span>
+        </button>
+        <Link href="#">
+          <div>
+            <img
+              src="https://lh3.googleusercontent.com/aida-public/AB6AXuCxOYrA8x__3bham69-kGm2hkfZhyXPvF3wKOg56Sdv-tqRX4UzZUyFd8fI3qOJ_MNaVo_Gm5ORsESKZXx7p1T2AHnHfmvZmKWjnhA7JmyX-mAh0RE0BoEP_CIyRMxC80jvv-BMyvJHLy8DYhVnhOepisgNw_nlcs_vUa_OEYybBE43Xagh80VlYj4iE0h38NX4qlfDCwSl0v1knMHuL-XucE9NjhYYupH-wCWaULS3_XG7YiUVO5cDPwUgrla39tKbqJ_Tcdmwk-o"
+              alt="User Avatar"
+              className="w-10 h-10 rounded-full object-cover"
+            />
+          </div>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
